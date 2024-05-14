9e5851f39c89a268912fd7e698db707dd7295928
Tsedeniya Zewdu <tsedeniyazewdu116@gmail.com>
2024-05-14 16:17:06 +0300

    my portfolio

---
+++ main.js
@@ -0,0 +1,10 @@
+var typed = new Typed('.text', {
+  strings: [
+    'a Fullstack Developer and',
+    'Website Development and Database Adminstration Trainer ',
+  ],
+  typeSpeed: 50,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+})
