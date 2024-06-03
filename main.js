3ca6155727577d2e8a863804a377652fb45c6e9b
Tsedeniya Zewdu <tsedeniyazewdu116@gmail.com>
2024-06-03 18:34:28 +0300

    responsive

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 var typed = new Typed('.text', {
-  strings: [
-    'a Fullstack Developer and',
-    'Website Development and Database Adminstration Trainer ',
-  ],
+  strings: ['a Fullstack Developer'],
   typeSpeed: 50,
   backSpeed: 100,
   backDelay: 1000,
   loop: true,
 })
+function myFunction() {
+  var element = document.querySelector('.navbar')
+  element.classList.toggle('open')
+}
